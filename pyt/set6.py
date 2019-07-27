b2fe9262bbbe23d1a2ac75772f721266e0279dfa
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-07-27 12:28:32 +0300

    Update set6.py

--- pyt/set6.py
+++ pyt/set6.py
@@ -1,11 +1,14 @@
 def kume_dondur(str_kume: str):
-    if str_kume.startswith("{") and str_kume.endswith("}"):
-        kume_elemanlari = str_kume[1:len(str_kume) -1]
-        elemanlar = kume_elemanlari.split(", ")
-        return {int(eleman) for eleman in elemanlar}
-    else:
-        raise ValueError("Geçerli bir küme değil")
-        
-kume = input("küme girin: ")       
+        if str_kume.startswith("{") and str_kume.endswith("}"):
+            kume_elemanlari = str_kume[1:len(str_kume) -1]
+            elemanlar = kume_elemanlari.split(", ")
+            try:
+                return {int(eleman) for eleman in elemanlar}
+            except ValueError:
+                raise ValueError("Küme elemanları geçerli değil")
+        else:
+            raise ValueError("Geçerli bir küme değil")
+           
+kume = input("küme girin: ")      
 yeni_kume = kume_dondur(kume)
 print(yeni_kume, type(yeni_kume))
