f00d472bb88ab748f496427c58f4c13dda01924d
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-07-27 11:33:27 +0300

    Create set4.py

---
+++ pyt/set4.py
@@ -0,0 +1,7 @@
+def kume_dondur(str_kume: str):
+    if str_kume.startswith("{") and str_kume.endswith("}"):
+        kume_elemanlari = str_kume[1:len(str_kume) -1]
+        v = kume_elemanlari.split(", ")
+        print(v)
+       
+kume_dondur("{1, 2, 3, 17}")
