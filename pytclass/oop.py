4b6b4199d356a94f33bf7c438accacde2d7f4784
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-07-28 19:53:34 +0300

    Create oop.py
    Her 3 classta message = "Hello World" diye yazılıp ekrana aynı çıktıyı verir.

---
+++ pytclass/oop.py
@@ -0,0 +1,18 @@
+class Calisan1:
+    message = "Hello World"
+    def __init__(self):
+        print(self.message)
+
+class Calisan():
+    message = "Hello World"
+    def __init__(self):
+        print(self.message)
+
+class Calisan2(object):
+    message = "Hello World"
+    def __init__(self):
+        print(self.message)
+
+c1 = Calisan()
+c2 = Calisan1()
+c3 = Calisan2()
