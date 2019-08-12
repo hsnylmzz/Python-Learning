e00e6ef46f17e9de1181ae44837d179e6711f98a
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-08-12 12:30:55 +0300

    Create isimbilgisi.py

---
+++ pytlearning/isimbilgisi.py
@@ -0,0 +1,5 @@
+name = input("Adını Gir: ")
+surname = input("Soyadını Gir: ")
+okul_numarasi = input("Okul Numarasını Gir: ")
+
+print("Adı : {}\n Soyadı: {}\n Okul Numaranız: {} ".format(name,surname,okul_numarasi))
